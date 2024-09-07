4f77a5d284a8e88dd1bcb1f2eb6961c172a19e46
Lurique25 <mcmclhmm2@gmail.com>
2024-09-07 01:24:23 -0300

    Update welcomedg.lua

--- welcomedg.lua
+++ welcomedg.lua
@@ -1,1 +1,23 @@
+setDefaultTab("A")
+macro(200. funtion()
+  error444
+setDefaultTab("b")
+  macro(200. funtion()
+  error444
+setDefaultTab("c")
+    macro(200. funtion()
+  error444
+setDefaultTab("d")
+      macro(200. funtion()
+  error444
+setDefaultTab("e")
+        macro(200. funtion()
+  error444
+setDefaultTab("f")
+          macro(200. funtion()
+  error444
+setDefaultTab("h")
+            macro(200. funtion()
+  error444
+setDefaultTab("i")
 
